bfce66ef869c35fdbf79ccdaeeea0e10a8b88cb3
Shavinda Perera <126375057+shavinda11@users.noreply.github.com>
2023-02-26 11:55:38 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -6,15 +6,15 @@ from pathlib import Path
 
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = 'shavinda_p@hotmail.com'
-email['to'] = 'ishadi.up@gmail.com'
+email['from'] = 'sender's email'
+email['to'] = 'email of the person sending'
 email['subject'] = 'You won my love!'
 
-email.set_content(html.substitute(name = 'Ishadi ❤❤❤'), 'html')
+email.set_content(html.substitute(name = 'description'), 'html')
 
 with smtplib.SMTP(host='smtp.Outlook.com', port = 587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('shavinda_p@hotmail.com', 'imalawyer2023')
+    smtp.login('user-email', 'password')
     smtp.send_message(email)
     print('all good boss')
